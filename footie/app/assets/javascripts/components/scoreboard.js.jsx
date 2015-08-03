e110bdbbd5efc366a81a2be6a2c446190f2f36ae
cjmanetta <cjmanetta@gmail.com>
2015-08-03 08:04:16 -0700

    add working scoreboard component

--- footie/app/assets/javascripts/components/scoreboard.js.jsx
+++ footie/app/assets/javascripts/components/scoreboard.js.jsx
@@ -1,17 +1,19 @@
 var Scoreboard = React.createClass({
+
   render: function() {
     return (
       <div className="scoreboard small-6 columns">
-        <h2>scoreboard</h2>
-          <ul id="scoreboard-listing">
-            <li>test</li>
-          </ul>
+        {this.props.initialScores}
       </div>
     );
   }
 });
 
+
+
+
+
 React.render(
   <Scoreboard />,
-  document.body
+  $('.scores')
   )
--- footie/app/controllers/users_controller.rb
+++ footie/app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
+require 'json'
+
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @players = User.players
     @score = Score.new
     @activities = Activity.all
+    @totals_array = []
+    @total_scores = @players.map {|player| player.total_score}
+
+    @players.each_with_index do |player, index|
+      player_hash = {"firstname" => @players[index].firstname, "score" => @total_scores[index]  }
+      @totals_array << player_hash
+    end
+
+
 
     if @user.admin == true
       render 'users/coaches/dashboard'
@@ -12,3 +23,4 @@ class UsersController < ApplicationController
     end
   end
 end
+

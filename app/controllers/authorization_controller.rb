5fe657b0a79b4bbc509ffbb8b198dbb9c523279d
cjmanetta <cjmanetta@gmail.com>
2016-01-19 11:33:27 -0800

    add conditional check on rails environment before setting redirect uri value

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -5,15 +5,21 @@ class AuthorizationController < ApplicationController
 
 
   def initiate_oauth
-    # redirect_uri = "https://footie.herokuapp.com/oauth/exchange"
-    redirect_uri = "http://localhost:3000/oauth/exchange"
+    if Rails.env.development?
+      redirect_uri = "http://localhost:3000/oauth/exchange"
+    else
+    redirect_uri = "https://footie.herokuapp.com/oauth/exchange"
+    end
     redirect_to "https://auth.teamsnap.com/oauth/authorize?client_id=#{ENV['CLIENT_ID']}&redirect_uri=#{redirect_uri}&response_type=code"
   end
 
   def exchange_oauth
     auth_code = params[:code]
-    # redirect_uri = "https://footie.herokuapp.com/users#{current_user.id}"
-    redirect_uri = "http://localhost:3000/users#{current_user.id}"
+    if Rails.env.development?
+      redirect_uri = "http://localhost:3000/users#{current_user.id}"
+    else
+      redirect_uri = "https://footie.herokuapp.com/users#{current_user.id}"
+    end
     url = "https://auth.teamsnap.com/oauth/token?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&redirect_uri=#{redirect_uri}&code=#{auth_code}&grant_type=authorization_code"
     response_from_teamsnap = Net::HTTP.post_form(URI(url), {})
     data = JSON.parse(response_from_teamsnap.body)

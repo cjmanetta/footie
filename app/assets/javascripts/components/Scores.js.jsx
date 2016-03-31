b87eb02d2e1bb6b2fd3d2b07d8e475f9fce7f959
cjmanetta <cjmanetta@gmail.com>
2016-03-31 13:34:06 -0700

    add actions, store, reducer and roots

---
+++ app/assets/javascripts/components/Scores.js.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Scoreboard from './scoreboard.js.jsx';
+// import ScoreForm from './scoreform.js.jsx';
+
+ 
+const Scores = () => (
+	<div>
+		hello world
+	</div>
+);
+
+export default Scores;
---
+++ app/assets/javascripts/reducers/Scores.js
@@ -0,0 +1,69 @@
+import { combineReducers } from 'redux';
+import { addNewScore, postScore } from '../actions';
+import ReduxThunk from 'redux-thunk';
+
+const score = (state, action) => {
+	switch (action.type) {
+		case 'ADD_SCORE':
+			return {
+				id: action.id,
+				value: action.value,
+				player: action.player,
+				challenge: action.challenge
+			};
+		default:
+			return state;
+	}
+};
+
+const initialState = {
+	scores: [],
+	players: [],
+	challenges: [],
+	selectorCache: {
+		player: null, 
+		challenge: null
+	}
+};
+
+const scoresApp = (state =	initialState, action) => {
+	switch(action.type) {
+		case 'RECEIVE_CHALLENGES':
+			return Object.assign({}, state, { challenges: action.payload.challenges });
+		case 'RECEIVE_PLAYERS':
+			return Object.assign({}, state, { players: action.payload.players });
+		case 'RECEIVE_SCORES':
+			return Object.assign({}, state, { scores: action.payload.scores });
+		case 'SELECT_PLAYER':
+			return {
+				...state,
+				selectorCache: {
+					...state.selectorCache,
+					player: action.id,
+					firstname: action.firstname
+				}
+			};
+		case 'SELECT_CHALLENGE':
+			return {
+				...state,
+				selectorCache: {
+					...state.selectorCache,
+					challenge: action.id
+				}
+			};
+		case 'ADD_SCORE':
+			postScore(action, state)
+			return {
+				...state,
+				scores: addNewScore(state, action),
+				selectorCache: {
+					player: null, 
+					challenge: null
+				}
+			};
+		default:
+			return state;
+	}
+};
+
+export default scoresApp;

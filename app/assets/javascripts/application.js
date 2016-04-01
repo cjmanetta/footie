f0b7f1d1d0b24a268f5fee7e81d29848c48ff0d9
cjmanetta <cjmanetta@gmail.com>
2016-04-01 16:05:17 -0700

    fix semantic jquery menu toggle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,25 @@
 //= require_self
 //= require react-server
 //= require react_ujs
+//= require semantic
+
+
 
 window.$ = window.jQuery = global.$ = require('jquery');
 require( 'jquery-ujs' );
-var React = window.React = global.React = require('react');
 
-require('./components');
+// if were are on the landing page, load the jquery from semantic to hide and show the legacy login/signup pages else load react and friends
+
+if (window.location.pathname == "/") {
+	$(window).load(function(){
+		$('.menu .item').tab();
+		$('.ui.dropdown').dropdown();
+	})
+} else {
+	var React = window.React = global.React = require('react');
+	require('./components');
+}
+
+
+
+

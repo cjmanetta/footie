7c26bd1608fc48ece0a03ca4a16a48bcefbffbaa
cjmanetta <cjmanetta@gmail.com>
2016-06-27 11:46:48 -0700

    fix store filename in root

--- app/assets/javascripts/components/Root.js.jsx
+++ app/assets/javascripts/components/Root.js.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import configureStore from '../store';
+import configureStore from '../Store.js';
 import Scores from './scores.js.jsx';
 
 const store = configureStore();
